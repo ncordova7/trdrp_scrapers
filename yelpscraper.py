e805fcecdf7ef15e2d5a685d96fab3d65571aada
Suleiman Karkoutli <karko101@mail.chapman.edu>
2019-11-17 19:16:10 -0800

    Added Yelp Review Scraper

--- yelpscraper.py
+++ yelpscraper.py
@@ -57,7 +57,46 @@ desired_features = {"alias" :1, "url": 2, "coordinates.longitude": 3, "coordinat
          "location.state": 10, "location.zip_code" : 11, "name": 12,
         "phone": 13, "price": 14, "rating":15, "review_count" : 17,
         }
+#----Get Reviews for dispensaires
+desired_features_reviews = {"id" :1, "url": 2, "rating": 3, "text" : 4,
+        "url": 5, "id": 6, "user.id": 7
+        }
 
+def editReviews(df, desired_features, store_name):
+
+    source = "yelp"
+    d = datetime.datetime.today()
+    #loop through defined deatures and if a column is not there create one and put null values
+    for i in desired_features:
+            if(i not in df.columns):
+                df[i] = None
+
+    #drop columns not desired
+    for i in df.columns:
+        if(i not in desired_features):
+            df = copy.deepcopy(df.drop(columns = [i]))
+
+    #rename columns
+    df = copy.deepcopy(df.rename(columns={'user.id': 'user_id', 'id': 'review_id',
+                                          'location.city': 'city',
+                                          'url': 'review_url',
+                                         }))
+
+    #add date and source columns
+    df['Store_name'] = store_name
+    df['SOURCE'] = source
+    df['DATE_SCRAPED'] = d.strftime("%m-%d-%Y")
+
+    return df
+
+def getReviews(id, desired_features, store_name):
+    url = "https://api.yelp.com/v3/businesses/" + id +"/reviews"
+    req = requests.get(url, headers=headers)
+    reviews = req.json()
+    df1 = pd.DataFrame.from_dict(json_normalize(reviews['reviews']), orient = 'columns')
+    df = editReviews(df1, desired_features, store_name)
+
+    return df
 
 #-----Main Code -----
 
@@ -65,9 +104,23 @@ testDF = createRequest(headers, api_key, url, params)
 test1 = editYelpData(testDF, desired_features)
 d = datetime.datetime.today()
 
+test1Names = copy.deepcopy(test1)
+reviews = pd.DataFrame([])
+for index, row in test1Names.iterrows():
+    id = row['id']
+    store_name = row['name']
+
+    df2 = getReviews(id, desired_features_reviews, store_name)
+    reviews = copy.deepcopy(reviews.append(df2))
+    print(id, store_name)
+
+
 #write csv file
 filename = "yelp_" + d.strftime("%m-%d-%Y") + ".csv"
 test1.to_csv(r"output/" + filename, header = True, index = False)
+
+filename1 = "yelp_reviews_" + d.strftime("%m-%d-%Y") + ".csv"
+reviews.to_csv(r"output/" + filename1, header = True, index = False)
 
 #connect to database
 #database connection isnt working currently
